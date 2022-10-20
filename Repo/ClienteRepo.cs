2a63991d7d2fd33ab9b6b36580301c4c295d641d
francoarieltorres <chiqui_nick17@hotmail.com>
2022-10-19 23:01:51 -0300

    Agregar archivos de proyecto.

---
+++ Repo/ClienteRepo.cs
@@ -0,0 +1,70 @@
+﻿using MiAppMVC.Data;
+using MiAppMVC.Interfaces;
+using MiAppMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiAppMVC.Repo
+{
+    public class ClienteRepo : IClienteRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClienteRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Cliente> InicioCliente()
+        {
+            var listaCliente = _context.Cliente.ToList(); // select * from Cliente
+           
+
+            ///CONECTARA A LA BASE DE DATOS
+            ///ABRIR LA BASE DE DATOS
+            ///HACER UN SELECT * FROM CLIENTES
+            ///RETORNE ESE SELECT Y GUARDARLO EN UN DATASET
+            ///ESE DATASATE PASARLO A UN DATAGRID 
+            ///ME MUESTRE EN LA GRILLA DEL FRONT
+            ///CIERRO BASE DE DATOS
+
+            return listaCliente;
+        }
+
+        public Cliente DetalleCliente(int? id)
+        {
+
+            var cliente = _context.Cliente.FirstOrDefault(m => m.IdCliente == id);  // Select * from Cliente where idCliente= id
+                            //base de datos = _context
+                            //Cliente es la tabla de mi bd
+                            //fistOrDefault es un metodo que trae el primer elemento que encontro
+                            //m = ala tabla
+                            //m.Idcliente es el campo de mi tabla
+                            //Select * from Cliente where idCliente = id
+
+            return cliente;
+        }
+        public Cliente CrearCliente(Cliente cliente)
+        {
+            var obj = _context.Cliente.FirstOrDefault(m => m.Documento == cliente.Documento);
+            
+            if (obj == null) //verifico que el CLIENTE que voy a dar de alta NO EXISTE
+            {
+                cliente.Estado = true; // asigno estado del cliente con true para decir que esta activo
+
+                _context.Add(cliente);
+                _context.SaveChanges();
+                obj = cliente;
+
+                return obj;
+            }
+
+
+            return cliente;
+            
+        }
+    }
+}
